141b8abab8c7e7432787f355677302ad1a313a82
Ayomide <jimioniay@gmail.com>
2019-08-20 16:49:56 +0100

    feature/implement backend call

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [banks, setBanks] = useState({
+    status: false,
+    banks: [],
+  });
+
+  useEffect(() => {
+    fetchBanks();
+  }, []);
+
+  const fetchBanks = async () => {
+    try {
+      const response = await axios.get('/api/v1/transfer/getBanks');
+      setBanks({
+        status: response.status,
+        bankList: response.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(banks);
+
   return (
     <div className="App">
       <header className="App-header">
